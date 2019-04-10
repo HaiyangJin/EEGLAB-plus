37af25475467d5e5c5617c2756f8835ea1aca392
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-10 15:40:31 +1200

    add 'channel' input, remove 'isacc' input
    removed commented codes

--- erp_functions/st_trialdata.m
+++ erp_functions/st_trialdata.m
@@ -1,7 +1,9 @@
-function  trialEpochTable = st_trialdata(EEG, respEvent, blockEvent, isacc)
+function  trialEpochTable = st_trialdata(EEG, channels, onsetEvent, respEvent, blockEvent)
 % EEG: from eeglab
-% event(cell): event of the epochs
+% channels: which channels would you like to output (double or cell)
+% onsetEvent(cell): event of the onsets
 % respEvent(cell): event of the reponses
+% blockEvent(cell): events of the blocks
 % isacc(logical): do you include accuracy data?
 
 % Only works for individual EEG dataset (not study)
@@ -10,22 +12,27 @@ function  trialEpochTable = st_trialdata(EEG, respEvent, blockEvent, isacc)
 if nargin < 1
     error('Not enough inputs for eeg_trialdata!');
 end
-if nargin < 3 || isempty(respEvent) || isempty(blockEvent)
+if nargin <2 || isempty(channels)
+    channels = 1:length(EEG.chanlocs);
+else
+    channels = channame(channels); % covert anything to cell
+    channels = sort(cellfun(@(x) str2double(x(2:end)), channels)); % convert cell to numbers
+end
+if nargin < 3 || isempty(onsetEvent) || isempty(respEvent) || isempty(blockEvent)
     events = unique({EEG.event.type});
 end
-if nargin < 2
-    %     respEvent = {'RES0' 'RES1'};
-    respEvent = events(cellfun(@(x) strcmp(x(1:2), 'RE'), events));
-end
 if nargin < 3
-    blockEvent = events(cellfun(@(x) strcmp(x(1:3), 'blo'), events));
+    onsetEvent = events(cellfun(@(x) strcmp(x(end), '+'), events));
 end
 if nargin < 4
-    isacc = 1;
+    respEvent = events(cellfun(@(x) strcmp(x(1:2), 'RE'), events));
+end
+if nargin < 5
+    blockEvent = events(cellfun(@(x) strcmp(x(1:3), 'blo'), events));
 end
 
 % check if there is RT (latnecy) information for the events
-isRT = logical(sum(ismember(respEvent, {EEG.event.type})));
+isResp = logical(sum(ismember(respEvent, {EEG.event.type})));
 
 % check if there is block information for the events
 isBlock = logical(sum(ismember(blockEvent, {EEG.event.type})));
@@ -36,61 +43,68 @@ nTrial = EEG.trials;
 lagFrame = 1000 / EEG.srate;
 varNames = xconverter(EEG.xmin * 1000 : lagFrame : EEG.xmax * 1000);
 
-
 %% get the information about each trial
-
-if isRT
+if isResp
     latency = eeg_getepochevent(EEG, respEvent,[],'latency');  % the latency of the response events
 else
     warning('Resposne data are not available.');
 end
 
+% Get the original event number
+urevent = eeg_getepochevent(EEG, onsetEvent,[],'urevent');  % remaining epochs
+urTrialInfo = st_urtrialinfo(EEG, onsetEvent, respEvent, blockEvent);  % get the original information about each trial
+
 % IV
-Channel = {EEG.urchanlocs.labels}';
+allChannel = {EEG.urchanlocs.labels}';
+Channel = allChannel(channels);
 nRow = length(Channel);
 
 trialEpochTableIV = table;
 trialEpochTableDV = table;
 
-for iTrial = 1:nTrial
+for iEpoch = 1:nTrial
     % DV
     % data for this trial
-    thisTrialData = EEG.data(:,:,iTrial);
+    thisTrialData = EEG.data(channels,:,iEpoch);
     
     % IV
-    theEvents = EEG.epoch(iTrial).eventtype;
-    isOnset = cellfun(@(x) x(end) == '+', theEvents);
+    thisUrevent = urevent(iEpoch);
+    theEvents = EEG.epoch(iEpoch).eventtype;
+    isOnset = ismember(theEvents, onsetEvent);
     if sum(isOnset) > 1
         warning(['There are more than one onset events in this epoch!\n'...
             'filename: %s'], EEG.setname);
     end
     Event = repmat(theEvents(isOnset), nRow, 1);
+    Urevent = repmat(thisUrevent, nRow, 1);
+    
+    thisUrTrialInfo = urTrialInfo(ismember(urTrialInfo.Urevent, thisUrevent), :);
+    
+    TrialNumber = repmat(thisUrTrialInfo{1, 'TrialNumber'}, nRow, 1);
     
     if isBlock
-        isBlockTemp = cellfun(@(x) strcmp(x(1:3), 'blo'), theEvents);
+        isBlockTemp = ismember(theEvents, blockEvent);
         blockStr = theEvents{isBlockTemp};
         Block = repmat({blockStr(regexp(blockStr, '\d'))}, nRow, 1);
     else
         Block = repmat({''}, nRow, 1);
     end
         
-    if isacc % if there is accuracy data
-        isResp = cellfun(@(x) strcmp(x(1:2), 'RE'), theEvents);
-        if any(isResp) % Accuracy
-            respStr = theEvents{isResp};
+    if isResp
+        isRespHere = ismember(theEvents, respEvent);
+        if any(isRespHere) % Response
+            respStr = theEvents{isRespHere};
             Response = repmat({respStr(regexp(respStr, '\d'))}, nRow, 1);  % only get the numbers
+            RT = repmat(latency(1, iEpoch), nRow, 1);
         else
             Response = repmat({''}, nRow, 1);
+            RT = NaN(nRow, 1);
         end
+        urResponse = repmat(thisUrTrialInfo{1, 'urResponse'}, nRow, 1);
+        urRT = repmat(thisUrTrialInfo{1, 'urRT'}, nRow, 1);
     end
     
-    if isRT  % Response times
-        RT = repmat(latency(1, iTrial), nRow, 1);
-    else
-        RT = NaN(nRow, 1);
-    end
-    
-    thisIVTable = table(Channel, Event, Block, Response, RT);
+    thisIVTable = table(Channel, Event, Urevent, TrialNumber, Block, Response, urResponse, RT, urRT);
     thisDVTable = array2table(thisTrialData, 'VariableNames', varNames);
     
     trialEpochTableIV = vertcat(trialEpochTableIV, thisIVTable); %#ok<AGROW>
@@ -101,63 +115,5 @@ end
 trialEpochTableIV.SubjCode = repmat({EEG.setname(1:4)}, size(trialEpochTableIV, 1), 1);
 
 trialEpochTable = horzcat(trialEpochTableIV, trialEpochTableDV);
-
-
-%% get information from EEG (backup)
-% nChan = EEG.nbchan;
-% nPoint = EEG.pnts;
-% 
-% 
-% %% get the information about each trial
-% trialVarNames = {'SubjCode', 'Channel', 'Event', 'Response', 'RT'};
-% 
-% if isRT
-%     latency = eeg_getepochevent( EEG, respEvent,[],'latency');  % the latency of the response events
-%     if ~isacc, trialVarNames = trialVarNames(1, [1:3, 5]); end
-% else
-%     trialVarNames = trialVarNames(1, 1:3);
-%     warning('Resposne data are not available.');
-% end
-% 
-% trialData = zeros(nChan * nTrial, nPoint);
-% trialInfo = cell(nChan * nTrial, length(trialVarNames));
-% 
-% 
-% for iTrial = 1:nTrial
-%     % xIndex for this trial
-%     thisTrial = (iTrial-1)*nChan + (1:nChan);
-%     
-%     % data for this trial
-%     trialData(thisTrial, :) = EEG.data(:,:,iTrial);
-%     
-%     % channel for this trial
-%     trialInfo(thisTrial, 2) = {EEG.urchanlocs.labels}';
-%     
-%     % Event type for this trial
-%     theEvents = EEG.epoch(iTrial).eventtype;
-%     trialInfo(thisTrial, 3) = theEvents(1); % Independent Variable
-%     
-%     if isacc % if there is accuracy data
-%         isResp = cellfun(@(x) strcmp(x(1:2), 'RE'), theEvents);
-%         if any(isResp) % Accuracy
-%             respStr = theEvents{isResp};
-%             
-%             trialInfo(thisTrial, 4) = {regexp(respStr, '\d')};
-%         else
-%             trialInfo(thisTrial, 4) = {NaN};
-%         end
-%     end
-%     
-%     if isRT, trialInfo(thisTrial, 5) = {latency(1, iTrial)}; end% Response times
-%     
-% end
-% 
-% trialInfo(:, 1) = {EEG.setname(1:4)}; % save the participant code
-% 
-% % convert trial information and trial data into table
-% trialInfoTable = cell2table(trialInfo, 'VariableNames', trialVarNames);
-% trialDataTable = array2table(trialData, 'VariableNames', varNames);
-% 
-% trialEpochTable = horzcat(trialInfoTable, trialDataTable);
 
 end

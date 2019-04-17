eb5965ca7da347d80cd545757d3db88222d513cd
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-17 12:15:01 +1200

    pool data for MeanAmp and BinEpochERP separately

--- erp_functions/pool_stdata.m
+++ erp_functions/pool_stdata.m
@@ -1,6 +1,6 @@
 function pool_stdata(filepath, fn_outPre)
 
-if nargin < 1
+if nargin < 1 || isempty(filepath)
     filepath = uigetdir(pwd, 'Please select the path where the single trial data are stored.');
     cd(filepath);
 else
@@ -9,37 +9,51 @@ end
 binFiles = dir('*BinEpoch.mat');
 ampFiles = dir('*MeanAmp.mat');
 
+isBinFile = ~isempty(binFiles);
+isAmpFile = ~isempty(ampFiles);
+
 if nargin < 2 || isempty(fn_outPre)
-    fn_outPre = ampFiles(1).name(1:3); % expCode
+    if isBinFile
+        fn_outPre = binFiles(1).name(1:3); % expCode
+    else
+        fn_outPre = ampFiles(1).name(1:3); % expCode
+    end
 end
 
-% load and save bin epoch table
-bintable = table;
-for iBin = 1:size(binFiles, 1)
-    load(binFiles(iBin).name); 
-    bintable = [bintable; ST_BinEpochTable];  %#ok<AGROW>
-    clear ST_BinEpochTable
+%% Process bin erp files
+if isBinFile
+    % load and save bin epoch table
+    bintable = table;
+    for iBin = 1:size(binFiles, 1)
+        load(binFiles(iBin).name);
+        bintable = [bintable; ST_BinEpochTable];  %#ok<AGROW>
+        clear ST_BinEpochTable
+    end
+    
+    % save the erp data
+    output_binerp(bintable, [], [], 0, 0, fn_outPre);
+    output_binerp(bintable, [], [], 1, 0, fn_outPre);
+    fprintf('Bin epoch data are saved successfully for %s!\n', fn_outPre);
 end
 
-% save the erp data
-output_binerp(bintable, [], [], 0, 0, fn_outPre);
-output_binerp(bintable, [], [], 1, 0, fn_outPre);
 
-
-% load and save mean amplitude table
-amptable = table;
-for iAmp = 1:size(ampFiles, 1)
-    load(ampFiles(iAmp).name);
-    amptable = [amptable; ST_MeanAmpTable]; %#ok<AGROW>
-    clear ST_MeanAmpTable
+%% Process mean amp files
+if isAmpFile
+    % load and save mean amplitude table
+    amptable = table;
+    for iAmp = 1:size(ampFiles, 1)
+        load(ampFiles(iAmp).name);
+        amptable = [amptable; ST_MeanAmpTable]; %#ok<AGROW>
+        clear ST_MeanAmpTable
+    end
+    
+    % Save csv
+    % binfn = [expCode '_BinEpoch.csv'];
+    ampfn = [fn_outPre '_MeanAmp.csv'];
+    
+    % writetable(bintable, binfn);
+    writetable(amptable, ampfn);
+    fprintf('Mean amplitude data are saved successfully for %s!\n', fn_outPre);
 end
-
-%% Save csv
-% binfn = [expCode '_BinEpoch.csv'];
-ampfn = [fn_outPre '_MeanAmp.csv'];
-
-% writetable(bintable, binfn);
-writetable(amptable, ampfn);
-fprintf('Tables are saved successfully for %s!', fn_outPre);
 
 end

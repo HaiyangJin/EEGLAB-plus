74ee51cebaf7c8e24acbb3b745692d4ada66b4fc
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-19 19:38:10 +1200

    Create topo video for E204, E205, and E204 with Resp

--- erp_functions/TopoVideo/create_topovideo_72.m
+++ erp_functions/TopoVideo/create_topovideo_72.m
@@ -1,6 +1,7 @@
-function create_topovideo_24(videoNames, windowPlot, windowTable)
-% Create topo video for E204 and E205. 2 rows and 4 columns (without
-% responses)
+function create_topovideo_72(videoNames, windowPlot, windowTable, isResp)
+% Create topo video without responses for E204 and E205. 
+% Create topo vidoe with responses for E204 only
+
 
 if nargin < 1 || isempty(videoNames)
     videoNames = 'TopoVideo';
@@ -15,21 +16,41 @@ else
     lag = windowTable{1, 'StartTime'} / windowTable{1, 'StartFrame'};
     TW = windowTable{:, {'StartTime', 'EndTime'}};
 end
+if nargin < 4 || isempty(isResp)
+    isResp = 0;
+end
+if isResp
+    duration = {'17 ms inc', '17 ms cor', '200 ms'};
+    response = {'RES0', 'RES1'};
+    dura_72 = '772';
+    respLabel = 'Resp';
+    xColorbar = .9414;
+    screenRatio = .45;
+else
+    duration = {'17 ms', '200 ms'};
+    dura_72 = '72';
+    respLabel = 'All';
+    xColorbar = .92;
+    screenRatio = .52;
+end
 
 %% Record video
-v = VideoWriter([videoNames '_' num2str(windowPlot(1)) '_' num2str(windowPlot(2))], 'MPEG-4');
+v = VideoWriter([videoNames '_' respLabel '_' num2str(windowPlot(1)) '_' num2str(windowPlot(2))], 'MPEG-4');
 v.Quality = 100;
 open(v);
 
 %% The layout of the video
-vidSize = [50 50 1350 750];
+winX = 1600;
+winY = round(winX * screenRatio);
+winStart = [50, 50];
+vidSize = [winStart(1) winStart(2) winStart(1)+winX winStart(2)+winY];
 f = figure('NumberTitle', 'off', 'Position', vidSize);
 
 type = {'Intact', 'Scrambled'};
 category = {'Face', 'House'};
-duration = {'17 ms', '200 ms'};
+nDura = length(duration);
 nRow = length(type);
-nColu = length(category) * length(duration);
+nColu = length(category) * nDura;
 
 xStart = .1;
 xEnd = 0;
@@ -50,7 +71,7 @@ for iTime = windowPlot(1):lag:windowPlot(2)
     % Texts on the frame
     % Timepoint
     annotation(f,'textbox',...
-        [xPosi(1)+.05 yPosi(1)-.1 0.0 0.0],'String',{[num2str(iTime,'%0.0f') ' ms']},...
+        [xPosi(1)+.025 yPosi(1)-.05 0.0 0.0],'String',{[num2str(iTime,'%0.0f') ' ms']},...
         'HorizontalAlignment','center','FitBoxToText','on','FontSize',34,'LineStyle','-');
     % mean(xPosi(length(xPosi)/2 +([.5, 1.5])))
     
@@ -64,25 +85,18 @@ for iTime = windowPlot(1):lag:windowPlot(2)
     
     % Category (Columns)
     annotation(f,'textbox',...
-        [mean(xPosi([2,3])) yPosi(1)-.075 0.0 0.0],'String',category(1),...
+        [mean(xPosi(2:(nColu/2 + 1))) yPosi(1)-.075 0.0 0.0],'String',category(1),...
         'HorizontalAlignment','center','FitBoxToText','on','FontSize',28,'LineStyle','none');
     annotation(f,'textbox',...
-        [mean(xPosi([4,5])) yPosi(1)-.075 0.0 0.0],'String',category(2),...
+        [mean(xPosi((nColu/2 + 2):nColu+1)) yPosi(1)-.075 0.0 0.0],'String',category(2),...
         'HorizontalAlignment','center','FitBoxToText','on','FontSize',28,'LineStyle','none');
     
     % Duration (Columns)
-    annotation(f,'textbox',...
-        [xPosi(2) yPosi(1)-.15 0.0 0.0],'String',duration(1),...
-        'HorizontalAlignment','center','FitBoxToText','on','FontSize',22,'LineStyle','none');
-    annotation(f,'textbox',...
-        [xPosi(3) yPosi(1)-.15 0.0 0.0],'String',duration(2),...
-        'HorizontalAlignment','center','FitBoxToText','on','FontSize',22,'LineStyle','none');
-    annotation(f,'textbox',...
-        [xPosi(4) yPosi(1)-.15 0.0 0.0],'String',duration(1),...
-        'HorizontalAlignment','center','FitBoxToText','on','FontSize',22,'LineStyle','none');
-    annotation(f,'textbox',...
-        [xPosi(5) yPosi(1)-.15 0.0 0.0],'String',duration(2),...
-        'HorizontalAlignment','center','FitBoxToText','on','FontSize',22,'LineStyle','none');
+    for iDura = 1:nColu
+        annotation(f,'textbox',...
+            [xPosi(iDura+1) yPosi(1)-.15 0.0 0.0],'String',duration(mod(iDura-1, nDura)+1),...
+            'HorizontalAlignment','center','FitBoxToText','on','FontSize',22,'LineStyle','none');
+    end
     
     % Name of component
     if iTime > TW(1, 1) && iTime < TW(1, 2)
@@ -96,27 +110,32 @@ for iTime = windowPlot(1):lag:windowPlot(2)
         compColor = 'black';
     end
     annotation(f,'textbox',...
-        [xPosi(1)+.05 yPosi(1)-.175 0.0 0.0],'String',compName,'color',compColor,...
+        [xPosi(1) yPosi(1)-.13 0.0 0.0],'String',compName,'color',compColor,...
         'HorizontalAlignment','center','FitBoxToText','on','FontSize',32,'LineStyle','none');
     
     % topo map 
     fn_tmp = xconverter(iTime);
     fn_frame = fn_tmp{1};
     type_NS = 'NS';
-    dura_72 = '72';
     
     for iRow = 1:nRow 
         thisType = type_NS(iRow);
         
         for iColu = 1:nColu
-            thisCat = category{ceil(iColu/2)}(1);
-            thisDura = dura_72(mod(iColu-1, 2)+1);
+            tmpCol = mod(iColu-1, nDura)+1;
+            thisCat = category{ceil(iColu/nDura)}(1);
+            thisDura = dura_72(tmpCol);
         
             thisEvent = [thisType thisCat thisDura '+'];
             
-            thisFrameTopo = [thisEvent '_' fn_frame '.png'];
-            
-            thisfn = dir([thisEvent filesep thisFrameTopo]);
+            if isResp && thisDura == '7'
+                thisResp = response{tmpCol};
+                thisFrameTopo = [thisEvent '_' thisResp '_' fn_frame '.png'];
+                thisfn = dir([thisEvent filesep thisResp filesep thisFrameTopo]);        
+            else
+                thisFrameTopo = [thisEvent '_' fn_frame '.png'];
+                thisfn = dir([thisEvent filesep thisFrameTopo]);
+            end
             
             [rgb, ~, alpha] = imread([thisfn.folder filesep thisfn.name]);
             thisPosi = [xPosi(iColu + 1) - xDist*.45 yPosi(iRow + 1) - yDist*.7 ...
@@ -138,7 +157,7 @@ for iTime = windowPlot(1):lag:windowPlot(2)
     
     % settings for color bar
     colorLimit = [-5 5];
-    colorbar('Position', [.92 .135  0.025  .64],...
+    colorbar('Position', [xColorbar  .135  0.025  .64],...  % .92  .9414
         'FontSize', 19,...
         'Ticks', colorLimit(1):colorLimit(2));
     caxis(colorLimit);

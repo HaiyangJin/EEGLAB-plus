a4a58145c630e2a483affc9facf71e2915020b95
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-21 22:34:00 +1200

    correct the correction again...

--- erp_functions/st_analysis.m
+++ erp_functions/st_analysis.m
@@ -120,7 +120,7 @@ rawTrialTable = st_trialmulti('.', channels); %
 % correct the RT, as there is a offset of about 33ms (comparing with
 % E-prime results)
 rawTrialTable.RTBackup = rawTrialTable.RT;
-rawTrialTable.RT = rawTrialTable.urRT + 33;
+rawTrialTable.RT = rawTrialTable.urRT + 33 - 50;
 
 if isDenoise
     fprintf('\nPreparing for denoising the raw trial erp data...\n');

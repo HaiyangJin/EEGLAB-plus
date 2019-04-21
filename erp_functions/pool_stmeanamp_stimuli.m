d0ebdd50ae8886f9c1047a5a4b05d60ebb5be9e1
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-21 12:20:58 +1200

    pool single trial mean amplitude data with stimuli together

---
+++ erp_functions/pool_stmeanamp_stimuli.m
@@ -0,0 +1,12 @@
+function pool_stmeanamp_stimuli(ST_table, behavior_table)
+% behavior_table should include a column called trialNum
+error('To be finished...');
+
+% reorder the behavioral table (with trial number (order))
+
+
+
+
+
+
+end

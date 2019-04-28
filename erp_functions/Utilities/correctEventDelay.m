2d0a4b439b7231ed753d4999252696e2705bb0b4
Haiyang <hjin317@aucklanduni.ac.nz>
2019-04-28 22:15:02 +1200

    correct the latency delay for the events

--- erp_functions/Utilities/correctEventDelay.m
+++ erp_functions/Utilities/correctEventDelay.m
@@ -1,10 +1,19 @@
-function EEG = correctTriggerLatency(EEG,delaySize)
+function EEG = correctEventDelay(EEG, delaySize, events)
 %Takes the EEG structure and an onset delay (in ms), and moves all triggers
 %forward by the onset delay.
 %Note: Every trigger gets adjusted which may not be appropriate if different
 %events have different latencies eg. responses probably don't have
 %the same delay as visual stimuli so if you care about the timing of
 %reponses then probably don't use this.
+
+allEvents = {EEG.urevent.type};
+if nargin < 3 || isempty(events)
+    isOnset = cellfun(@(x) strcmp(x(4), '+'), allEvents);
+%     onsetEvents = allEvents(isOnset);
+else
+    isOnset = cellfun(@(x) ismember(x, events), allEvents);
+end 
+
 
 %adjusts the delay in ms to number of data points, accounting for
 %sampling rate
@@ -13,5 +22,9 @@ adjustedDelaySize = delaySize/samplingRateFix;
 
 %moves trigger latency by required number of data points
 for j = 1:size(EEG.event,2)
-    EEG.event(j).latency = EEG.event(j).latency + adjustedDelaySize;
+    if isOnset(j) 
+        EEG.event(j).latency = EEG.event(j).latency + adjustedDelaySize;
+    end
 end
+
+EEG.event.latency = ([EEG.urevent.latency] + adjustedDelaySize*isOnset)';

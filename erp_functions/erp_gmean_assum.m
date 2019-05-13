1581f76efd06f0ec01f81b78387266a8a4b3d4d2
Haiyang <hjin317@aucklanduni.ac.nz>
2019-05-13 14:36:31 +1200

    add more information about default channels used

--- erp_functions/erp_gmean_assum.m
+++ erp_functions/erp_gmean_assum.m
@@ -1,8 +1,8 @@
 function gmeanTable = erp_gmean_assum(epoch_table, trialNum_table, chanCentTable, isCluster)
 
 if nargin < 3
-    chanCentTable = {'P1', '65', '90';
-                     'N170', '58', '96'};
+    chanCentTable = {'P1', '65', '90'; % PO7 and PO8 (from previous literature)
+                     'N170', '58', '96'}; % P7 (T5) and P8 (T6) (from previous literature)
     fprintf('\nThe default channels are used for P1 and N170.\n');
 end
 if nargin < 4
